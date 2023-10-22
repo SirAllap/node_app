4819e3ebf48ba7f0e2709acf2b88f5a856eaa708
David Pallarés <serallapdivad@gmail.com>
2023-10-22 16:39:58 +0100

    localhost displays the .txt file data

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 const port = 3000;
+const data = fs_1.default.readFileSync('file/countries.txt', {
+    encoding: 'utf8',
+    flag: 'r',
+});
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('this is the txt file: ' + data);
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Example app listening on port ${port}`);
+    console.log(data);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express, { Express, Request, Response } from 'express'
+import fs from 'fs'
 const app: Express = express()
 const port = 3000
 
+const data = fs.readFileSync('file/countries.txt', {
+	encoding: 'utf8',
+	flag: 'r',
+})
+
 app.get('/', (req: Request, res: Response) => {
-	res.send('Hello World!')
+	res.send('this is the txt file: ' + data)
 })
 
 app.listen(port, () => {
 	console.log(`⚡️[server]: Example app listening on port ${port}`)
+	console.log(data)
 })
