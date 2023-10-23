078022e91ce327462d8810c1546be81c0679af2b
David Pallarés <serallapdivad@gmail.com>
2023-10-23 17:10:15 +0100

    Clean index.ts file. - Fresh start with a basic express server running

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
 import express, { Express, Request, Response } from 'express'
-import fs from 'fs'
 const app: Express = express()
 const port = 3000
 
-const data = fs.readFileSync('file/countries.txt', {
-	encoding: 'utf8',
-	flag: 'r',
-})
-
 app.get('/', (req: Request, res: Response) => {
-	res.send('this is the txt file: ' + data)
+	res.send('I am alive!')
 })
 
 app.listen(port, () => {
 	console.log(`⚡️[server]: Example app listening on port ${port}`)
-	console.log(data)
 })
