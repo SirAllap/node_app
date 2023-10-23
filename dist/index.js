01ae41348346ae632ceb63dced8f78ae379369a1
David Pallarés <serallapdivad@gmail.com>
2023-10-23 17:12:04 +0100

    comopile new index.js

--- dist/index.js
+++ dist/index.js
@@ -4,18 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 const port = 3000;
-const data = fs_1.default.readFileSync('file/countries.txt', {
-    encoding: 'utf8',
-    flag: 'r',
-});
 app.get('/', (req, res) => {
-    res.send('this is the txt file: ' + data);
+    res.send('I am alive!');
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Example app listening on port ${port}`);
-    console.log(data);
 });
 //# sourceMappingURL=index.js.map
