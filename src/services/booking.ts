a93f0fc30be59b86ca43428d0e0c313f0030b811
David Pallarés <serallapdivad@gmail.com>
2023-10-24 16:39:11 +0100

    Create booking.ts service: Only 2 methods has been done

---
+++ src/services/booking.ts
@@ -0,0 +1,35 @@
+import bookingsData from '../data/bookings.json'
+import { IBooking } from '../models/booking'
+
+export const bookings = bookingsData as IBooking[]
+
+async function get() {
+	const result = await bookings
+	return result
+}
+
+async function getById(bookingId: number) {
+	const id = bookingId.toString()
+	const result = await bookings.filter((booking) => booking.id === id)
+	return result
+}
+
+async function post(booking: IBooking) {
+	// Save a booking to json file
+}
+
+async function put(bookingId: number, update: Partial<IBooking>) {
+	// Update a booking by id and save to json file
+}
+
+async function _delete(bookingId: number) {
+	// Delete a booking by id from json file
+}
+
+export const bookingService = {
+	get,
+	getById,
+	post,
+	put,
+	delete: _delete,
+}
