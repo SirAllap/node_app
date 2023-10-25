56bf5427bc33ef35f4730dea1e3ddf75ba502889
David Pallarés <serallapdivad@gmail.com>
2023-10-25 12:19:51 +0100

    added rooms middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { bookingsController } from './controllers/booking'
 import express, { Express, Request, Response, NextFunction } from 'express'
 import cors from 'cors'
+import { roomsController } from './controllers/room'
 
 export const app: Express = express()
 
@@ -9,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/bookings', bookingsController)
+app.use('/rooms', roomsController)
 
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
 	console.error(err)
