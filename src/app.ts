182ea779519c7e5ba804b7f58ee5ebff3f76abce
David Pallarés <serallapdivad@gmail.com>
2023-10-25 18:10:28 +0100

    Import the auth middleware to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import authMiddleware from './middlewares/auth'
 import { bookingsController } from './controllers/booking'
 import { roomsController } from './controllers/room'
 import { loginController } from './controllers/login'
@@ -10,11 +11,10 @@ export const app: Express = express()
 	.use(cors())
 	.use(express.json())
 	.use(morgan('combined'))
+	.use('/login', loginController)
+	.use(authMiddleware)
 	.use('/bookings', bookingsController)
 	.use('/rooms', roomsController)
-
-	.use('/login', loginController)
-
 	.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
 		console.error(err)
 		return res
