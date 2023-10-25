1f8f0b3a744d57ce94f9a3046eab7b71b79a3c68
David Pallarés <serallapdivad@gmail.com>
2023-10-25 10:28:01 +0100

    Added error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { bookingsController } from './controllers/booking'
-import express, { Express } from 'express'
+import express, { Express, Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 
 export const app: Express = express()
@@ -9,3 +9,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/bookings', bookingsController)
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+	console.error(err)
+	return res.status(500).json({ error: true, message: 'Application error' })
+})
