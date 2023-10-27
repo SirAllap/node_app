0bb59994a6503810d165ef7e9e7bc014b39b0282
David Pallarés <serallapdivad@gmail.com>
2023-10-27 19:19:23 +0100

    generated the booking schema from IBooking model

---
+++ src/models/booking.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose'
+import { IBooking } from './booking'
+
+const bookingSchema = new Schema<IBooking>({
+	guest: { type: String, required: true },
+	phone_number: { type: String, required: true },
+	order_date: { type: String, required: true },
+	check_in: { type: String, required: true },
+	check_out: { type: String, required: true },
+	special_request: { type: String, required: true },
+	room_type: { type: String, required: true },
+	room_number: { type: String, required: true },
+	status: { type: String, required: true },
+	photos: { type: [String], required: true },
+})
+
+export const bookingModel = model<IBooking>('booking', bookingSchema)
