e6995ddf74fd2c0f33a7b516aac4cac8453ef771
David Pallarés <serallapdivad@gmail.com>
2023-10-27 20:56:02 +0100

    Improved how to data is managed after request DB

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
 import { IUser } from '../models/user'
 import { userService } from '../services/user'
 
@@ -25,38 +25,49 @@ usersController.get(
 	}
 )
 
-usersController.post('/', async (req: Request<IUser>, res: Response) => {
-	try {
-		await userService.createOne(req.body)
-		res.json('User successfully created')
-	} catch (error) {
-		res.status(500).json(`${error}`)
+usersController.post(
+	'/',
+	async (req: Request<IUser>, res: Response, next: NextFunction) => {
+		try {
+			const result = await userService.createOne(req.body)
+			res.json(result)
+		} catch (error) {
+			next(error)
+		}
 	}
-})
+)
 
 usersController.put(
 	'/:userId',
-	async (req: Request<{ userId: number }, IUser>, res: Response) => {
+	async (
+		req: Request<{ userId: number }, IUser>,
+		res: Response,
+		next: NextFunction
+	) => {
 		try {
-			const id = req.params.userId
-			const userToUpdate = req.body
-			await userService.updateOne(id, userToUpdate),
-				res.json('User successfully updated')
+			const result = await userService.updateOne(
+				req.params.userId,
+				req.body
+			)
+			res.json(result)
 		} catch (error) {
-			res.status(500).json(`${error}`)
+			next(error)
 		}
 	}
 )
 
 usersController.delete(
 	'/:userId',
-	async (req: Request<{ userId: number }>, res: Response) => {
+	async (
+		req: Request<{ userId: number }>,
+		res: Response,
+		next: NextFunction
+	) => {
 		try {
-			const id = req.params.userId
-			await userService.destroyOne(id)
-			res.json('User successfully deleted')
+			const result = await userService.destroyOne(req.params.userId)
+			res.json(result)
 		} catch (error) {
-			res.status(500).json(`${error}`)
+			next(error)
 		}
 	}
 )
