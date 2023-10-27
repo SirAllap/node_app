f4a2419def5dddc9202f922047edebfab4097906
David Pallarés <serallapdivad@gmail.com>
2023-10-27 19:35:12 +0100

    generated the contact schema from IContact model

---
+++ dist/models/contact.model.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.contactModel = void 0;
+const mongoose_1 = require("mongoose");
+const contacSchema = new mongoose_1.Schema({
+    full_name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    subject_of_review: { type: String, required: true },
+    review_body: { type: String, required: true },
+    date: { type: String, required: true },
+    dateTime: { type: String, required: true },
+    isArchived: { type: String, required: true },
+});
+exports.contactModel = (0, mongoose_1.model)('contacts', contacSchema);
