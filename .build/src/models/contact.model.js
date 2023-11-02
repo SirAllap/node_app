449d9c97549225e6079d08145014ff60230ecbea
David Pallarés <serallapdivad@gmail.com>
2023-11-02 11:26:05 +0000

    Build updated

---
+++ .build/src/models/contact.model.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.contactModel = void 0;
+const mongoose_1 = require("mongoose");
+const contacSchema = new mongoose_1.Schema({
+    contact_id: { type: String, required: true },
+    full_name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    subject_of_review: { type: String, required: true },
+    review_body: { type: String, required: true },
+    date: { type: String, required: true },
+    dateTime: { type: String, required: true },
+    isArchived: { type: String, required: true },
+});
+exports.contactModel = (0, mongoose_1.model)('contacts', contacSchema);
