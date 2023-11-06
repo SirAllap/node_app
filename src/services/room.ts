8033241029c301b17b11c3b9928ed16eea61ae30
David Pallarés <serallapdivad@gmail.com>
2023-11-06 21:44:33 +0000

    Created the query to: - Get all rooms - Get one room - Delete one room

--- src/services/room.ts
+++ src/services/room.ts
@@ -1,18 +1,20 @@
 import roomsData from '../data/rooms.json'
 import { IRoom } from '../models/room'
+import { SelectQuery } from '../util/util'
 
 export const rooms = roomsData as IRoom[]
 
 const fetchAll = async () => {
-	const result = await rooms
-	if (!result) throw new Error('Error obtaining all rooms')
+	const result = await SelectQuery(
+		'SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities FROM room r LEFT JOIN photo p ON r.id = p.room_id LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id LEFT JOIN amenity a ON ahr.amenity_id = a.id GROUP BY r.id;'
+	)
 	return result
 }
 
 const fetchOne = async (roomId: number) => {
-	const id = roomId.toString()
-	const result = await rooms.filter((room) => room.id === id)
-	if (result.length === 0) throw new Error('Bad request')
+	const result = await SelectQuery(
+		`SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities FROM room r LEFT JOIN photo p ON r.id = p.room_id LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id LEFT JOIN amenity a ON ahr.amenity_id = a.id WHERE r.id = ${roomId} GROUP BY r.id;`
+	)
 	return result
 }
 
@@ -36,10 +38,7 @@ const updateOne = async (roomId: number, update: Partial<IRoom>) => {
 }
 
 const destroyOne = async (roomId: number) => {
-	const id = roomId.toString()
-	const currentObjectIndex = rooms.findIndex((room) => room.id === id)
-	if (currentObjectIndex === -1) throw new Error('Booking not found')
-	const result = await rooms.splice(currentObjectIndex, 1)
+	const result = await SelectQuery(`DELETE FROM room WHERE id = ${roomId};`)
 	return result
 }
 
