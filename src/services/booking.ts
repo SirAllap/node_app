ccd6a105dcf249745fb5b671f03f6b7fa09c396b
David Pallarés <serallapdivad@gmail.com>
2023-11-06 21:43:43 +0000

    Created the query to: - Get all bookings - Get one booking - Delete one booking

--- src/services/booking.ts
+++ src/services/booking.ts
@@ -1,18 +1,20 @@
 import bookingsData from '../data/bookings.json'
 import { IBooking } from '../models/booking'
+import { SelectQuery } from '../util/util'
 
 export const bookings = bookingsData as IBooking[]
 
 const fetchAll = async () => {
-	const result = await bookings
-	if (!result) throw new Error('Error obtaining all bookings')
+	const result = await SelectQuery(
+		'SELECT b.*, r.room_number, r.room_type, GROUP_CONCAT(p.photos) AS room_pictures FROM booking b LEFT JOIN room r ON b.room_id = r.id LEFT JOIN photo p ON r.id = p.room_id GROUP BY b.id, r.room_number, r.room_type;'
+	)
 	return result
 }
 
 const fetchOne = async (bookingId: number) => {
-	const id = bookingId.toString()
-	const result = await bookings.filter((booking) => booking.id === id)
-	if (result.length === 0) throw new Error('Bad request')
+	const result = await SelectQuery(
+		`SELECT b.*, r.room_number, r.room_type, GROUP_CONCAT(p.photos) AS room_pictures FROM booking b LEFT JOIN room r ON b.room_id = r.id LEFT JOIN photo p ON r.id = p.room_id WHERE b.id = ${bookingId} GROUP BY b.id, r.room_number, r.room_type;`
+	)
 	return result
 }
 
@@ -38,12 +40,9 @@ const updateOne = async (bookingId: number, update: Partial<IBooking>) => {
 }
 
 const destroyOne = async (bookingId: number) => {
-	const id = bookingId.toString()
-	const currentObjectIndex = bookings.findIndex(
-		(booking) => booking.id === id
+	const result = await SelectQuery(
+		`DELETE FROM booking WHERE id = ${bookingId};`
 	)
-	if (currentObjectIndex === -1) throw new Error('Booking not found')
-	const result = await bookings.splice(currentObjectIndex, 1)
 	return result
 }
 
