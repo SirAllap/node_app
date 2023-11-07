8b86023fe41a491f1abbcc48df8e0c82e1e7ceea
David Pallarés <serallapdivad@gmail.com>
2023-11-07 10:53:17 +0000

    Updated Interface to match SQL DB

--- src/models/booking.ts
+++ src/models/booking.ts
@@ -1,13 +1,10 @@
 export interface IBooking {
-	id: string
 	guest: string
 	phone_number: string
 	order_date: string
 	check_in: string
 	check_out: string
 	special_request: string
-	room_type: string
-	room_number: string
 	status: string
-	photos: string[]
+	room_id: number
 }
