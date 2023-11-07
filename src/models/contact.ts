837e583e200f697a815ce7273613222ee5104fcd
David Pallarés <serallapdivad@gmail.com>
2023-11-07 19:58:25 +0000

    Updated Interface to match SQL DB

--- src/models/contact.ts
+++ src/models/contact.ts
@@ -1,11 +1,9 @@
 export interface IContact {
-	id: string
 	full_name: string
 	email: string
 	phone_number: string
 	subject_of_review: string
 	review_body: string
 	date: string
-	dateTime: string
-	isArchived: string
+	status: string
 }
