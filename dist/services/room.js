4c2b9cfa467c7eb19629c117aa543afedd95a2e4
David Pallarés <serallapdivad@gmail.com>
2023-11-07 18:20:43 +0000

    Room.ts: Now queries are using parameterized queries to avoid SQL injections.

--- dist/services/room.js
+++ dist/services/room.js
@@ -17,30 +17,71 @@ const rooms_json_1 = __importDefault(require("../data/rooms.json"));
 const util_1 = require("../util/util");
 exports.rooms = rooms_json_1.default;
 const fetchAll = () => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield (0, util_1.SelectQuery)('SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities FROM room r LEFT JOIN photo p ON r.id = p.room_id LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id LEFT JOIN amenity a ON ahr.amenity_id = a.id GROUP BY r.id;');
+    const query = `
+	SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities
+	FROM room r LEFT JOIN photo p ON r.id = p.room_id
+	LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id
+	LEFT JOIN amenity a ON ahr.amenity_id = a.id
+	GROUP BY r.id;
+	`;
+    const result = yield (0, util_1.SelectQuery)(query);
     return result;
 });
 const fetchOne = (roomId) => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield (0, util_1.SelectQuery)(`SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities FROM room r LEFT JOIN photo p ON r.id = p.room_id LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id LEFT JOIN amenity a ON ahr.amenity_id = a.id WHERE r.id = ${roomId} GROUP BY r.id;`);
+    const query = `
+	SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities
+	FROM room r
+	LEFT JOIN photo p ON r.id = p.room_id
+	LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id
+	LEFT JOIN amenity a ON ahr.amenity_id = a.id WHERE r.id=?
+	GROUP BY r.id;
+	`;
+    const params = [roomId];
+    const result = yield (0, util_1.SelectQuery)(query, params);
     return result;
 });
 const createOne = (room) => __awaiter(void 0, void 0, void 0, function* () {
-    const currentRoomLenght = exports.rooms.length;
-    const result = yield exports.rooms.push(room);
-    if (currentRoomLenght === exports.rooms.length)
-        throw new Error('Error posting new room');
+    const query = `
+	INSERT INTO room (room_number, room_type, description, price, offer_price, discount, status)
+	VALUES (?, ?, ?, ?, ?, ?, ?);
+	`;
+    const params = [
+        room.room_number,
+        room.room_type,
+        room.description,
+        room.price,
+        room.offer_price,
+        room.discount,
+        room.status,
+    ];
+    const result = yield (0, util_1.ModifyQuery)(query, params);
     return result;
 });
 const updateOne = (roomId, update) => __awaiter(void 0, void 0, void 0, function* () {
-    const id = roomId.toString();
-    const currentObjectIndex = exports.rooms.findIndex((room) => room.id === id);
-    if (currentObjectIndex === -1)
-        throw new Error('Booking not found');
-    const result = (exports.rooms[currentObjectIndex] = Object.assign(Object.assign({}, exports.rooms[currentObjectIndex]), update));
+    const query = `
+	UPDATE room
+	SET room_number=?, room_type=?, description=?, price=?, offer_price=?, discount=?, status=?
+	WHERE id=?;
+	`;
+    const params = [
+        update.room_number,
+        update.room_type,
+        update.description,
+        update.price,
+        update.offer_price,
+        update.discount,
+        update.status,
+        roomId,
+    ];
+    const result = yield (0, util_1.ModifyQuery)(query, params);
     return result;
 });
 const destroyOne = (roomId) => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield (0, util_1.SelectQuery)(`DELETE FROM room WHERE id = ${roomId};`);
+    const query = `
+	DELETE FROM room WHERE id=?;
+	`;
+    const params = [roomId];
+    const result = yield (0, util_1.SelectQuery)(query, params);
     return result;
 });
 exports.roomService = {
--- src/services/room.ts
+++ src/services/room.ts
@@ -1,44 +1,79 @@
 import roomsData from '../data/rooms.json'
 import { IRoom } from '../models/room'
-import { SelectQuery } from '../util/util'
+import { ModifyQuery, SelectQuery } from '../util/util'
 
 export const rooms = roomsData as IRoom[]
 
 const fetchAll = async () => {
-	const result = await SelectQuery(
-		'SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities FROM room r LEFT JOIN photo p ON r.id = p.room_id LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id LEFT JOIN amenity a ON ahr.amenity_id = a.id GROUP BY r.id;'
-	)
+	const query = `
+	SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities
+	FROM room r LEFT JOIN photo p ON r.id = p.room_id
+	LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id
+	LEFT JOIN amenity a ON ahr.amenity_id = a.id
+	GROUP BY r.id;
+	`
+	const result = await SelectQuery(query)
 	return result
 }
 
 const fetchOne = async (roomId: number) => {
-	const result = await SelectQuery(
-		`SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities FROM room r LEFT JOIN photo p ON r.id = p.room_id LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id LEFT JOIN amenity a ON ahr.amenity_id = a.id WHERE r.id = ${roomId} GROUP BY r.id;`
-	)
+	const query = `
+	SELECT r.*, GROUP_CONCAT(DISTINCT p.photos) AS all_photos, GROUP_CONCAT(a.amenities) AS all_amenities
+	FROM room r
+	LEFT JOIN photo p ON r.id = p.room_id
+	LEFT JOIN amenities_has_room ahr ON r.id = ahr.room_id
+	LEFT JOIN amenity a ON ahr.amenity_id = a.id WHERE r.id=?
+	GROUP BY r.id;
+	`
+	const params = [roomId]
+	const result = await SelectQuery(query, params)
 	return result
 }
 
 const createOne = async (room: IRoom) => {
-	const currentRoomLenght = rooms.length
-	const result = await rooms.push(room)
-	if (currentRoomLenght === rooms.length)
-		throw new Error('Error posting new room')
+	const query = `
+	INSERT INTO room (room_number, room_type, description, price, offer_price, discount, status)
+	VALUES (?, ?, ?, ?, ?, ?, ?);
+	`
+	const params = [
+		room.room_number,
+		room.room_type,
+		room.description,
+		room.price,
+		room.offer_price,
+		room.discount,
+		room.status,
+	]
+	const result = await ModifyQuery(query, params)
 	return result
 }
 
 const updateOne = async (roomId: number, update: Partial<IRoom>) => {
-	const id = roomId.toString()
-	const currentObjectIndex = rooms.findIndex((room) => room.id === id)
-	if (currentObjectIndex === -1) throw new Error('Booking not found')
-	const result = (rooms[currentObjectIndex] = {
-		...rooms[currentObjectIndex],
-		...update,
-	})
+	const query = `
+	UPDATE room
+	SET room_number=?, room_type=?, description=?, price=?, offer_price=?, discount=?, status=?
+	WHERE id=?;
+	`
+	const params = [
+		update.room_number,
+		update.room_type,
+		update.description,
+		update.price,
+		update.offer_price,
+		update.discount,
+		update.status,
+		roomId,
+	]
+	const result = await ModifyQuery(query, params)
 	return result
 }
 
 const destroyOne = async (roomId: number) => {
-	const result = await SelectQuery(`DELETE FROM room WHERE id = ${roomId};`)
+	const query = `
+	DELETE FROM room WHERE id=?;
+	`
+	const params = [roomId]
+	const result = await SelectQuery(query, params)
 	return result
 }
 
