d666aaa26ba5eebf013f90f0b9536f9217795d94
David Pallarés <serallapdivad@gmail.com>
2023-11-07 18:20:00 +0000

    Updated Interface to match SQL DB

--- src/models/room.ts
+++ src/models/room.ts
@@ -1,11 +1,7 @@
 export interface IRoom {
 	room_number: string
-	id: string
-	room_photo: string[]
 	room_type: string
 	description: string
-	amenities_type: string
-	amenities: [{ name: string; description: string }]
 	price: number
 	offer_price: boolean
 	discount: number
