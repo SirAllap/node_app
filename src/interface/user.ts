1ae4d316cb38385521e0dee27c918797fb1d7907
David Pallarés <serallapdivad@gmail.com>
2023-11-08 19:51:42 +0000

    Modified the user interface to match validation

--- src/interface/user.ts
+++ src/interface/user.ts
@@ -2,7 +2,7 @@ export interface IUser {
 	full_name: string
 	email: string
 	photo: string
-	start_date: string
+	start_date: Date | string
 	description: string
 	phone_number: string
 	status: string
