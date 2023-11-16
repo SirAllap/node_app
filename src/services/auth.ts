cbdbd6b12a4e5215d53a9b56bc2ade6498875d15
David PR <serallapdivad@gmail.com>
2023-11-16 09:14:57 +0000

    Now the payload send some user info

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -5,15 +5,28 @@ import { UserModel } from '../models/user.model'
 
 const secret: string = process.env.SECRET || ''
 
+interface IUserInfo {
+	email: string
+	name: string
+	role: string
+	photo: string
+}
+
 const login = async (email: string, password: string) => {
 	const result = await UserModel.findOne({ email: email })
 	if (!result) throw new Error()
 	const passwordCheck = await bcrypt.compare(password, result.password || '')
 	if (!passwordCheck) throw new Error()
-	return signJWT({ email })
+	const userInfo: IUserInfo = {
+		email: result.email,
+		name: result.full_name,
+		role: result.description,
+		photo: result.photo,
+	}
+	return signJWT({ userInfo })
 }
 
-const signJWT = (payload: { email: string }) => {
+const signJWT = (payload: { userInfo: IUserInfo }) => {
 	const token = jwt.sign(payload, secret, { expiresIn: '5h' })
 	return { payload, token }
 }
